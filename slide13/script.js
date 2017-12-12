3915a4e243beda5d97990e781772f6d9add51004
Thomas Koch <e1526232@student.tuwien.ac.at>
2017-12-12 21:32:17 +0100

    draw circles around ability markers

--- slide13/script.js
+++ slide13/script.js
@@ -1,46 +1,26 @@
-var abilityMarkers = '{ "mercy":[ {"x":0.1,"y":0.3,"text":"6x Q"}, {"x":0.2,"y":0.3,"text":"6x Q"}, {"x":0.5,"y":0.7,"text":"6x LSHIFT"} ], "soldier":[ {"x":0.2,"y":0.3,"text":"8x Q"}, {"x":0.9,"y":0.3,"text":"6x Q"}, {"x":0.5,"y":0.7,"text":"6x E"} ] }';
-var jsonMarkers = JSON.parse(abilityMarkers)["mercy"];
+var abilityMarkers = '{ "hanamura": { "mercy":[ {"x":0.1,"y":0.3,"text":"6x Q","radius":1.0}, {"x":0.2,"y":0.3,"text":"6x Q","radius":0.5}, {"x":0.5,"y":0.7,"text":"6x LSHIFT","radius":0.3} ], "soldier":[ {"x":0.2,"y":0.3,"text":"8x Q","radius":0.2}, {"x":0.9,"y":0.3,"text":"6x Q","radius":0.9}, {"x":0.5,"y":0.7,"text":"6x E","radius":0.8} ]}, "ilios": {"mercy":[ {"x":0.1,"y":0.3,"text":"6x Q","radius":1.0}, {"x":0.2,"y":0.3,"text":"6x Q","radius":0.5}, {"x":0.5,"y":0.7,"text":"6x LSHIFT","radius":0.3} ], "soldier":[ {"x":0.2,"y":0.3,"text":"8x Q","radius":0.2}, {"x":0.9,"y":0.3,"text":"6x Q","radius":0.9}, {"x":0.5,"y":0.7,"text":"6x E","radius":0.8} ]} }';
+var jsonMarkers = JSON.parse(abilityMarkers)["hanamura"]["mercy"];
 
 function drawShortMarker(value) {
-	var svgMarker = $("<svg width='300' height='200'><polygon points='0,0 100,0 100,40 50,60 0,40' style='fill:#218ffe;' /><text fill='#e3f2ff' font-size='30' x='20' y='32' font-family='overwatch'>" + value.text + "</text></svg>").css({
+	var svgMarker = $("<svg width='300' height='300'><circle cx='100' cy='100' r='" + value.radius * 70 + "' stroke='#218ffe' stroke-width='2' fill='none' stroke-dasharray='10' /><polygon points='50,50 150,50 150,90 100,110 50,90' style='fill:#218ffe;' /><text fill='#e3f2ff' font-size='30' x='70' y='82' font-family='overwatch'>" + value.text + "</text></svg>").css({
 		'transform': 'translate(' + $(window).width() * value.x + 'px,' + $(window).height() * value.y + 'px)'
 	});
 	$("body").append(svgMarker);
 }
 
 function drawLongMarker(value) {
-	var svgMarker = $("<svg width='300' height='200'><polygon points='0,0 150,0 150,40 75,60 0,40' style='fill:#218ffe;' /><text fill='#e3f2ff' font-size='30' x='14' y='32' font-family='overwatch'>" + value.text + "</text></svg>").css({
+	var svgMarker = $("<svg width='300' height='200'><circle cx='125' cy='100' r='" + value.radius * 70 + "' stroke='#218ffe' stroke-width='2' fill='none' stroke-dasharray='10' /><polygon points='50,50 200,50 200,90 125,110 50,90' style='fill:#218ffe;' /><text fill='#e3f2ff' font-size='30' x='64' y='82' font-family='overwatch'>" + value.text + "</text></svg>").css({
 		'transform': 'translate(' + $(window).width() * value.x + 'px,' + $(window).height() * value.y + 'px)'
 	});
 	$("body").append(svgMarker);
 }
 
-$("#map").change(function() {
-});
-
-$("#heroSelect").change(function() {
-	$("#abilities").empty();
-	$("svg").remove();
-	if ($("#heroSelect").val() == "mercy") {
-		jsonMarkers = JSON.parse(abilityMarkers)["mercy"];
-		$("#abilities").append("<input type='checkbox' value='q'> Q - Valkyrie<br />");
-		$("#abilities").append("<input type='checkbox' value='e'> E - Resurrect<br />");
-		$("#abilities").append("<input type='checkbox' value='lshift'> LSHIFT - Guardian Angel<br />");
-	} else {
-		jsonMarkers = JSON.parse(abilityMarkers)["soldier"];
-		$("#abilities").append("<input type='checkbox' value='q'> Q - Tactical Visor<br />");
-		$("#abilities").append("<input type='checkbox' value='e'> E - Biotic Field<br />");
-		$("#abilities").append("<input type='checkbox' value='lshift'> LSHIFT - Sprint<br />");
-	}
-});
-
-$("#abilities").change(function() {
+function placeMarkers() {
 	$("svg").remove();
 	$("#abilities input:checked").each(function() {
 		var selectedCheckboxName = $(this).attr("value");
 		$.each(jsonMarkers, function(index, value) {
 			if (value.text.toLowerCase().includes(selectedCheckboxName)) {
-				console.log(selectedCheckboxName);
 				if (selectedCheckboxName == "lshift" || selectedCheckboxName == "rshift") {
 					drawLongMarker(value);
 				} else {
@@ -49,6 +29,30 @@ $("#abilities").change(function() {
 			}
 		});
 	});
+}
+
+$("#mapSelect").change(function() {
+	placeMarkers();
+});
+
+$("#heroSelect").change(function() {
+	$("#abilities").empty();
+	$("svg").remove();
+	jsonMarkers = JSON.parse(abilityMarkers)[$("#mapSelect").val()][$("#heroSelect").val()];
+	if ($("#heroSelect").val() == "mercy") {
+		$("#abilities").append("<input type='checkbox' value='q'> Q - Valkyrie<br />");
+		$("#abilities").append("<input type='checkbox' value='e'> E - Resurrect<br />");
+		$("#abilities").append("<input type='checkbox' value='lshift'> LSHIFT - Guardian Angel<br />");
+	} else {
+		$("#abilities").append("<input type='checkbox' value='q'> Q - Tactical Visor<br />");
+		$("#abilities").append("<input type='checkbox' value='e'> E - Biotic Field<br />");
+		$("#abilities").append("<input type='checkbox' value='lshift'> LSHIFT - Sprint<br />");
+	}
+});
+
+$("#abilities").change(function() {
+	jsonMarkers = JSON.parse(abilityMarkers)[$("#mapSelect").val()][$("#heroSelect").val()];
+	placeMarkers();
 });
 
 $("#navBarCloseButton").click(function() {
